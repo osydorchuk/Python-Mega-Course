05ecb6a6f3d18cd683f784a06c213a735eb661d1
Oleksandr Sydorchuk <a.sydorchuk@gmail.com>
2024-04-08 10:53:17 +0300

    add edit

--- app1/main.py
+++ app1/main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -12,7 +12,13 @@ while True:
             for item in todos:
                 item = item.title()
                 print(item)
+        case 'edit':
+            number = int(input("number of the to doto edit: "))
+            number = number - 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
         case 'exit':
+            existing_todo = todos[number]
             break
         case _:
             print("Hey, you entered an unknown command")
