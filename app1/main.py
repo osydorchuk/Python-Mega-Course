7f3b6c0aad8e2bf01a3809b294148b3c13275deb
Oleksandr Sydorchuk <a.sydorchuk@gmail.com>
2024-04-09 11:13:48 +0300

    add write from and to file

--- app1/main.py
+++ app1/main.py
@@ -1,13 +1,20 @@
-todos = []
-
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     match user_action:
         case 'add':
-            todo = input("Enter to do: ")
+            todo = input("Enter to do: ") + "\n"
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo.capitalize())
+
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
         case 'show' | 'display':
             for index, item in enumerate(todos):
                 row = f"{index + 1}-{item}"
@@ -23,7 +30,6 @@ while True:
             number = int(input("number of the todo to complete:: "))
             todos.pop(number - 1)
         case 'exit':
-            existing_todo = todos[number]
             break
         case _:
             print("Hey, you entered an unknown command")
