08ffca779db6122887fc7c7a40a365b3819719bd
Oleksandr Sydorchuk <ext.oleksandr.sydorchuk@ef.com>
2024-06-20 20:50:42 +0300

    add default-arguments

--- app1/main.py
+++ app1/main.py
@@ -1,10 +1,10 @@
-def get_todos(filepath):
+def get_todos(filepath='files/todos.txt'):
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath='files/todos.txt'):
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
 
@@ -16,14 +16,14 @@ while True:
     if user_action.startswith('add') or user_action.startswith('new'):
         todo = user_action[4:]
 
-        todos = get_todos(filepath='files/todos.txt')
+        todos = get_todos()
 
         todos.append(todo.capitalize() + '\n')
 
         write_todos(filepath='files/todos.txt', todos_arg=todos)
 
     elif user_action.startswith('show') | user_action.startswith('display'):
-        todos = get_todos('files/todos.txt')
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -35,13 +35,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines()
+            todos = get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            write_todos('files/todos.txt', todos)
+            write_todos(todos)
 
         except ValueError:
             print("Your command is not valid")
@@ -51,13 +50,13 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos('files/todos.txt')
+            todos = get_todos()
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos('files/todos.txt', todos)
+            write_todos(todos, 'files/todos.txt')
 
             message = f"todo {todo_to_remove} was removed from the list"
             print(message)
