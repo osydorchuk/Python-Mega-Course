877137afba41b1a4f5f9186cf2252ddb5768772a
Oleksandr Sydorchuk <ext.oleksandr.sydorchuk@ef.com>
2024-06-20 21:07:42 +0300

    add doc-strings

--- app1/main.py
+++ app1/main.py
@@ -1,14 +1,19 @@
 def get_todos(filepath='files/todos.txt'):
+    """Read a text file and return the list of
+    to-do items."""
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
 def write_todos(todos_arg, filepath='files/todos.txt'):
+    """Write the to-do list in the text file."""
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
 
 
+# print(help(get_todos))
+# print(help(write_todos))
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
